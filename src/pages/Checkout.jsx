44e74e0f5e2a75c280022d4b092f15bc358e6dbb
Anand Raj Singh <singhpariharanandraj@gmail.com>
2023-07-27 09:15:30 +0530

    Payment Form

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { Container, TextField } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { updatePayment } from "../feature/checkout-slice";
 
 export default function Checkout() {
+  
+
   return (
-    <div>
+    <Container>
       
-    </div>
+    </Container>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -24,31 +24,31 @@ export default function Login() {
         await signIn(email.value, password.value);
         navigate("/");
     }
-  return (
-    <Container component="main" maxWidth="x5">
-      <CssBaseline/>
-      <Box sx={{mt: theme.spacing(8), display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <Avatar sx={{m: 1, backgroundColor: theme.palette.secondary.main}}>
-            <LockOutlinedIcon/>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-            Sign In
-        </Typography>
-        <form onSubmit={login} sx={{width: "100%", mt: 1}}>
-            <TextField label="E-mail" variant="outlined" margin="normal" required fullWidth id="email" name="email" type="email" autoFocus autoComplete="off"/>
-            <TextField label="Password" variant="outlined" margin="normal" required fullWidth id="password" name="password" type="password" autoFocus autoComplete="current-password"/>
-            <Button type="submit" variant="contained" fullWidth color="primary" sx={{margin: theme.spacing(3, 0, 2)}}>
-                Sign In
-            </Button>
-        </form>
-        <Grid container justifyContent={"flex - end"}>
-            <Grid item>
-                <Link variant="body2" href="/register">
-                    New user? Sign Up
-                </Link>
-            </Grid>
-        </Grid>
-      </Box>
-    </Container>
-  )
+    return (
+        <Container component="main" maxWidth="x5">
+            <CssBaseline />
+            <Box sx={{ mt: theme.spacing(8), display: "flex", flexDirection: "column", alignItems: "center" }}>
+                <Avatar sx={{ m: 1, backgroundColor: theme.palette.secondary.main }}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign In
+                </Typography>
+                <form onSubmit={login} sx={{ width: "100%", mt: 1 }}>
+                    <TextField label="E-mail" variant="outlined" margin="normal" required fullWidth id="email" name="email" type="email" autoFocus autoComplete="off" />
+                    <TextField label="Password" variant="outlined" margin="normal" required fullWidth id="password" name="password" type="password" autoFocus autoComplete="current-password" />
+                    <Button type="submit" variant="contained" fullWidth color="primary" sx={{ margin: theme.spacing(3, 0, 2) }}>
+                        Sign In
+                    </Button>
+                </form>
+                <Grid container justifyContent={"flex - end"}>
+                    <Grid item>
+                        <Link variant="body2" href="/register">
+                            New user? Sign Up
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Container>
+    )
 }
